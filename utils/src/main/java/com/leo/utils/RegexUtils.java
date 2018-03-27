4333de520fe97f193624cfb068d2cdc47cf6f305
leo <firelion0725@163.com>
2018-03-27 14:39:57 +0800

    修正gradle 版本

--- utils/src/main/java/com/leo/utils/RegexUtils.java
+++ utils/src/main/java/com/leo/utils/RegexUtils.java
@@ -30,7 +30,7 @@ public class RegexUtils {
      * @param input 待验证文本
      * @return {@code true}: 匹配<br>{@code false}: 不匹配
      */
-    public static boolean isTel(final CharSequence input) {
+    public static boolean isTel(CharSequence input) {
         return isMatch(MOBILE_SIMPLE, input);
     }
 
@@ -40,7 +40,7 @@ public class RegexUtils {
      * @param input 待验证文本
      * @return {@code true}: 匹配<br>{@code false}: 不匹配
      */
-    public static boolean isIDCard18(final CharSequence input) {
+    public static boolean isIDCard18(CharSequence input) {
         return isMatch(REGEX_ID_CARD18, input);
     }
 
@@ -50,7 +50,7 @@ public class RegexUtils {
      * @param input 待验证文本
      * @return {@code true}: 匹配<br>{@code false}: 不匹配
      */
-    public static boolean isEmail(final CharSequence input) {
+    public static boolean isEmail(CharSequence input) {
         return isMatch(REGEX_EMAIL, input);
     }
 
@@ -61,7 +61,7 @@ public class RegexUtils {
      * @param input 要匹配的字符串
      * @return {@code true}: 匹配<br>{@code false}: 不匹配
      */
-    public static boolean isMatch(final String regex, final CharSequence input) {
+    public static boolean isMatch(String regex, CharSequence input) {
         return input != null && input.length() > 0 && Pattern.matches(regex, input);
     }
 }

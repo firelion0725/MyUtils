b8363193c27b9e3b08658b95b8c3ce88904185c6
leo <firelion0725@163.com>
2018-04-12 10:09:09 +0800

    增加注释

--- utils/src/main/java/com/leo/utils/StringUtils.java
+++ utils/src/main/java/com/leo/utils/StringUtils.java
@@ -7,6 +7,11 @@ package com.leo.utils;
 
 public class StringUtils {
 
+    /**
+     * 拼接字符串
+     * @param strs 需要拼接字符
+     * @return 拼接完成后字符
+     */
     public static String StringAppend(String... strs) {
         StringBuilder sb = new StringBuilder();
         for (String str : strs) {
@@ -15,6 +20,11 @@ public class StringUtils {
         return sb.toString();
     }
 
+    /**
+     * 拼接字符串
+     * @param objs 需要拼接字符
+     * @return 拼接完成后字符
+     */
     public static String StringAppend(Object... objs) {
         StringBuilder sb = new StringBuilder();
         for (Object str : objs) {
@@ -25,7 +35,7 @@ public class StringUtils {
 
     /**
      * 判断是否有特殊字符
-     * @param string
+     * @param string 需要判断字符
      * @return true：为正常字符（汉字+数字+大小写英文） false：有特殊字符
      */
     public static boolean isConSpeCharacters(String string) {

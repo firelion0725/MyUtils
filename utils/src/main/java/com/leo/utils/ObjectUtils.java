4313553145c4232ac44647150b742758f8affc14
leo <firelion0725@163.com>
2018-06-12 14:36:20 +0800

    1.增加一个防御式判断

---
+++ utils/src/main/java/com/leo/utils/ObjectUtils.java
@@ -0,0 +1,13 @@
+package com.leo.utils;
+
+/**
+ * @author leo, ZhangWei
+ * @date 2018/6/12
+ */
+public class ObjectUtils {
+
+    public static boolean checkNotNull(Object obj) {
+        return obj != null;
+    }
+
+}

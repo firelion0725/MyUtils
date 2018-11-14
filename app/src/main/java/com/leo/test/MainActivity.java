dfa2cb6fe949652744472872c73df4bd0cb69a48
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2018-11-14 16:25:55 +0800

    增加获取ip地址的方法

--- app/src/main/java/com/leo/test/MainActivity.java
+++ app/src/main/java/com/leo/test/MainActivity.java
@@ -16,9 +16,9 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 KeyboardUtils.toggleSoftInput(MainActivity.this)
         );
 
-        tv.setText(stringFromJNI());
+//        tv.setText(stringFromJNI());
         testThreadPool();
     }
 
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+//    public native String stringFromJNI();
 
     private void test() {
 
--- utils/src/main/java/com/leo/utils/DeviceUtils.java
+++ utils/src/main/java/com/leo/utils/DeviceUtils.java
@@ -10,7 +10,10 @@ import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.util.Enumeration;
 
 /**
@@ -129,5 +132,28 @@ public class DeviceUtils {
             return "";
         }
         return result;
+    }
+
+    /**
+     * 获取IP地址
+     * @return “”则为获取到
+     */
+    public static String getIpAddressString() {
+        try {
+            for (Enumeration<NetworkInterface> enNetI = NetworkInterface
+                    .getNetworkInterfaces(); enNetI.hasMoreElements(); ) {
+                NetworkInterface netI = enNetI.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = netI
+                        .getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (inetAddress instanceof Inet4Address && !inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
